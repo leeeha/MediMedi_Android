16f5d062a26cf9b0167303122b7b178f28d10e2a
leeeha <jxlhe46@seoultech.ac.kr>
2022-02-16 18:56:58 +0900

    프로젝트 이름 변경 MediMedi

--- app/src/main/java/com/gdsc/medimedi/fragment/ManualFragment.kt
+++ app/src/main/java/com/gdsc/medimedi/fragment/ManualFragment.kt
@@ -7,28 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import com.gdsc.medimedi.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ManualFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ManualFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    /*
+        안녕하세요. 여러분의 눈이 되어줄 메디메디 입니다.
+        화면을 한번 클릭하면 텍스트 모드,
+        두번 클릭하면 음성 모드로 앱을 사용할 수 있습니다.
+     */
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,25 +21,5 @@ class ManualFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_manual, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ManualFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ManualFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
